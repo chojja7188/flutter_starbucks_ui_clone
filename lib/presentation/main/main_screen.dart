94653263c2b337832b2e05ca481d847a4b686425
chojja7188 <shoun7188@naver.com>
2024-04-05 15:26:07 +0900

    feat: tab 구분

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_starbucks_ui_clone/presentation/component/header_component.dart';
+import 'package:flutter_starbucks_ui_clone/presentation/tab/home_tab.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,40 +9,7 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          HeaderComponent(),
-          SliverAppBar(
-            scrolledUnderElevation: 0,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text("What's New", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
-                  Text("Coupon", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
-                ],
-              ),
-              background: Column(
-                children: [
-
-                ],
-              )
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              childCount: 8, (BuildContext context, int index) {
-                return Container(
-                  color: Colors.red,
-                  height: 400,
-                  child: Text('Hi!'),
-                );
-            }
-            ),
-          )
-        ],
-      ),
+      body: HomeTab()
     );
   }
 }
---
+++ lib/presentation/tab/home_tab.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_starbucks_ui_clone/presentation/component/header_component.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        HeaderComponent(),
+        SliverAppBar(
+          scrolledUnderElevation: 0,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("What's New", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+                  Text("Coupon", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+                ],
+              ),
+              background: Column(
+                children: [
+
+                ],
+              )
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+              childCount: 8, (BuildContext context, int index) {
+            return Container(
+              color: Colors.red,
+              height: 400,
+              child: Text('Hi!'),
+            );
+          }
+          ),
+        )
+      ],
+    );
+  }
+}
